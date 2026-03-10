3eda8bdaffd6b3e7d4fd6d8fa60dfd6bb82e7906
AA-C-hub <aacc3@hawaii.edu>
2026-03-10 13:25:25 -1000

    carousel ; footer

--- js/script.js
+++ js/script.js
@@ -39,4 +39,20 @@ document.addEventListener("DOMContentLoaded", function () {
         panel.style.backgroundColor = activeTab.dataset.color;
     }
 
+
+    /* ----------------------------
+       ACCORDION CAROUSEL CODE
+    ---------------------------- */
+
+    const accordionItems = document.querySelectorAll(".accordion-item");
+
+    accordionItems.forEach(item => {
+        item.addEventListener("click", () => {
+
+            accordionItems.forEach(i => i.classList.remove("active"));
+            item.classList.add("active");
+
+        });
+    });
+
 });
